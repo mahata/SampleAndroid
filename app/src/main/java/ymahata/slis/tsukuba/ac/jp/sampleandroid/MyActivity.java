600503a4dc6affedb43c33d9426581d885bb96c6
Yasunori Mahata <mahata777@gmail.com>
2014-12-03 04:34:19 -0800

    Add TextWatcher

--- app/src/main/java/ymahata/slis/tsukuba/ac/jp/sampleandroid/MyActivity.java
+++ app/src/main/java/ymahata/slis/tsukuba/ac/jp/sampleandroid/MyActivity.java
@@ -2,22 +2,33 @@ package ymahata.slis.tsukuba.ac.jp.sampleandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
-public class MyActivity extends Activity {
+public class MyActivity extends Activity implements TextWatcher {
+
+    private static final String TAG = "MainActivity";
+    private EditText memo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        memo = (EditText) findViewById(R.id.editText);
+        memo.addTextChangedListener(this);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.my, menu);
+        // getMenuInflater().inflate(R.menu.my, menu);
         return true;
     }
 
@@ -31,5 +42,17 @@ public class MyActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        String memo = s.toString();
+        Log.d(TAG, "Checking for " + memo);
     }
 }
